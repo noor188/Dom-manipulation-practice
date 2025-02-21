a46d61d622966423860dc2f1c18b3e35e4b02412
noor188 <noormmlk@gmail.com>
2025-02-20 17:16:48 -0700

    Ticket 3 1-3

---
+++ todo.js
@@ -0,0 +1,47 @@
+// 1. Add a new task to the list when the "Add Task" button is clicked.
+
+// 1. setup: access elements
+const button = document.getElementById('add-task');
+const ul     = document.getElementById('todo-list');
+
+// 2. work : manipulation
+function createItem(){
+    const li = document.createElement('li');
+    li.textContent = 'Task 3';
+    ul.appendChild(li);
+}
+
+// 3. mouse event
+button.addEventListener('click', createItem);
+
+// 2. Remove the last task from the list when the "Remove Task" button is clicked.
+
+// 1. setup: access elements
+const removeButton = document.getElementById('remove-task');
+
+// 2. work : manipulation
+function removeItem(){
+    ul.removeChild(ul.lastElementChild);
+}
+
+// 3. mouse event
+removeButton.addEventListener('click', removeItem);
+
+// 3. Clear all tasks when the "Clear List" button is clicked.
+
+// 1. setup: access elements
+const clearButton = document.getElementById('clear-list');
+const allItems = document.querySelectorAll('li');
+
+// 2. work : manipulation
+function clearList(){
+    // for (let i = 0; i< allItems.length; i++){
+    //     allItems[i].remove();
+    // }
+    ul.innerHTML='';
+}
+
+// 3. mouse event
+clearButton.addEventListener('click', clearList);
+
+// 4. Toggle a 'completed' class on the list items when clicked.
